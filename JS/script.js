e3d39b4abddcf0c47fce16943a7b1f67229f13a9
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-07-25 14:58:25 -0300

    JavaScript part.

---
+++ JS/script.js
@@ -0,0 +1,98 @@
+let stock = {
+    cookie: 5,
+    candy: 10,
+    chocolate: 4,
+  };
+  
+  let soldProducts = {};
+  let refillProducts = {};
+  
+  let totalMoney = 20;
+  
+  function sellProduct() {
+    const product = document.getElementById("sell-product").value;
+    const quantity = parseInt(document.getElementById("sell-quantity").value);
+    const price = parseFloat(document.getElementById("sell-price").value);
+  
+    if (stock[product] >= quantity && quantity > 0 && price > 0) {
+      stock[product] -= quantity;
+      totalMoney += price * quantity;
+  
+      if (soldProducts[product]) {
+        soldProducts[product].quantity += quantity;
+        soldProducts[product].totalRevenue += price * quantity;
+      } else {
+        soldProducts[product] = { quantity, totalRevenue: price * quantity };
+      }
+  
+      updateStock();
+      updateSoldProducts();
+      updateTotalMoney();
+    } else {
+      alert("Invalid quantity, product not available, or invalid price!");
+    }
+  }
+  
+  function refillProduct() {
+    const product = document.getElementById("refill-product").value;
+    const quantity = parseInt(document.getElementById("refill-quantity").value);
+    const price = parseFloat(document.getElementById("refill-price").value);
+  
+    if (quantity > 0 && price > 0) {
+      stock[product] += quantity;
+      totalMoney -= price * quantity;
+  
+      if (refillProducts[product]) {
+        refillProducts[product].quantity += quantity;
+        refillProducts[product].totalCost += price * quantity;
+        refillProducts[product].pricePerItem = refillProducts[product].totalCost / refillProducts[product].quantity;
+      } else {
+        refillProducts[product] = { quantity, totalCost: price * quantity, pricePerItem: price };
+      }
+  
+      updateStock();
+      updateRefillProducts();
+      updateTotalMoney();
+  
+      // Clear the input fields after successfully refilling a product
+      document.getElementById("refill-quantity").value = "1";
+      document.getElementById("refill-price").value = "";
+    } else {
+      alert("Invalid quantity or price!");
+    }
+  }
+  
+  function updateStock() {
+    document.getElementById("stock-cookie").textContent = stock.cookie;
+    document.getElementById("stock-candy").textContent = stock.candy;
+    document.getElementById("stock-chocolate").textContent = stock.chocolate;
+  }
+  
+  function updateSoldProducts() {
+    const soldProductContainer = document.getElementById("sold-product-container");
+    soldProductContainer.innerHTML = "<h2>Sold Products</h2>";
+    for (const product in soldProducts) {
+      const { quantity, totalRevenue } = soldProducts[product];
+      const pricePerItem = totalRevenue / quantity;
+      soldProductContainer.innerHTML += `<p>${product}: ${quantity} sold for $${totalRevenue.toFixed(2)} ($${pricePerItem.toFixed(2)} each)</p>`;
+    }
+  }
+  
+  function updateRefillProducts() {
+    const refillProductContainer = document.getElementById("refill-product-container");
+    refillProductContainer.innerHTML = "<h2>Refilled Products</h2>";
+    for (const product in refillProducts) {
+      const { quantity, totalCost, pricePerItem } = refillProducts[product];
+      refillProductContainer.innerHTML += `<p>${product}: ${quantity} refilled for $${totalCost.toFixed(2)} ($${pricePerItem.toFixed(2)} each)</p>`;
+    }
+  
+    // Set a timeout to remove the product details after 2 seconds
+    setTimeout(() => {
+      refillProductContainer.innerHTML = "<h2>Refilled Products</h2>";
+    }, 2000); // 2000 milliseconds = 2 seconds
+  }
+  
+  function updateTotalMoney() {
+    document.getElementById("total-money").textContent = totalMoney.toFixed(2);
+  }
+  
