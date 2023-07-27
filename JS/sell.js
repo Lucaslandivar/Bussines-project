1a4ed42f62e9069d6d2247e71dfde6b7c23af1f5
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-07-27 11:48:54 -0300

    Updated the stock.js

--- JS/sell.js
+++ JS/sell.js
@@ -1,39 +1,39 @@
 // sell-container
 function sellProduct() {
-    const product = document.getElementById("sell-product").value;
-    const quantity = parseInt(document.getElementById("sell-quantity").value);
-    const price = parseFloat(document.getElementById("sell-price").value);
-  
-    if (quantity > 0 && price > 0) {
-      const productName = getProductFromStock(product);
-      const availableQuantity = stock[productName];
-  
-      if (availableQuantity >= quantity) {
-        const totalSaleAmount = price * quantity;
-  
-        stock[productName] -= quantity;
-        totalMoney += totalSaleAmount;
-  
-        if (soldProducts[productName]) {
-          soldProducts[productName].quantity += quantity;
-          soldProducts[productName].totalRevenue += totalSaleAmount;
-        } else {
-          soldProducts[productName] = { quantity, totalRevenue: totalSaleAmount };
-        }
-  
-        updateStock();
-        updateSoldProducts();
-        updateTotalMoney();
-        updateRefillProducts();
-  
-        // Store the updated data in Local Storage
-        localStorage.setItem("stock", JSON.stringify(stock));
-        localStorage.setItem("soldProducts", JSON.stringify(soldProducts));
-        localStorage.setItem("totalMoney", JSON.stringify(totalMoney));
+  const product = document.getElementById("sell-product").value;
+  const quantity = parseInt(document.getElementById("sell-quantity").value);
+  const price = parseFloat(document.getElementById("sell-price").value);
+
+  if (quantity > 0 && price > 0) {
+    const productName = getProductFromStock(product);
+    const availableQuantity = stock[productName];
+
+    if (availableQuantity >= quantity) {
+      const totalSaleAmount = price * quantity;
+
+      stock[productName] -= quantity;
+      totalMoney += totalSaleAmount;
+
+      if (soldProducts[productName]) {
+        soldProducts[productName].quantity += quantity;
+        soldProducts[productName].totalRevenue += totalSaleAmount;
       } else {
-        alert(`Not enough quantity in stock! Available quantity: ${availableQuantity}`);
+        soldProducts[productName] = { quantity, totalRevenue: totalSaleAmount };
       }
+
+      updateStock();
+      updateSoldProducts();
+      updateTotalMoney();
+      updateRefillProducts();
+
+      // Store the updated data in Local Storage
+      localStorage.setItem("stock", JSON.stringify(stock));
+      localStorage.setItem("soldProducts", JSON.stringify(soldProducts));
+      localStorage.setItem("totalMoney", JSON.stringify(totalMoney));
     } else {
-      alert("Invalid quantity or price!");
+      alert(`Not enough quantity in stock! Available quantity: ${availableQuantity}`);
     }
+  } else {
+    alert("Invalid quantity or price!");
   }
+}
--- JS/stock.js
+++ JS/stock.js
@@ -1,76 +1,81 @@
-// stock-container
 let stock = {
-    cookie: 5,
-    candy: 10,
-    chocolate: 4,
-  };
-  
-  // Get the correct product name from the stock
-  function getProductFromStock(product) {
-    for (const stockProduct in stock) {
-      if (stockProduct.toLowerCase() === product.toLowerCase()) {
-        return stockProduct;
-      }
-    }
-    return product.toLowerCase();
-  }
-  
-  // Delete a product from the stock
-  function deleteProduct(product) {
-    if (confirm(`Are you sure you want to delete "${capitalizeFirstLetter(product)}"?`)) {
-      delete stock[product];
-      updateStock();
-      updateSellProductOptions(); // Update options in the sell-product dropdown
-      updateRefillProducts();
-    }
-  }
-  
-  // Capitalize the first letter of a string
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  
-  // Update the stock list display
-  function updateStock() {
-    const stockList = document.getElementById("stock-list");
-    stockList.innerHTML = "";
-  
-    for (const product in stock) {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-        <span contenteditable="true" class="product-name" data-product="${product}">${capitalizeFirstLetter(product)}</span>: 
-        <span class="product-quantity" id="stock-${product}">${stock[product]}</span>
-        <button onclick="deleteProduct('${product}')">Remove</button>
-      `;
-      stockList.appendChild(listItem);
-    }
-  }
+  cookie: 5,
+  candy: 10,
+  chocolate: 4,
+};
 
-  // When the page loads, retrieve data from Local Storage if available
-    document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.getItem("stock")) {
-      stock = JSON.parse(localStorage.getItem("stock"));
-      updateStock();
+// Get the correct product name from the stock
+function getProductFromStock(product) {
+  for (const stockProduct in stock) {
+    if (stockProduct.toLowerCase() === product.toLowerCase()) {
+      return stockProduct;
     }
-  
-    if (localStorage.getItem("soldProducts")) {
-      soldProducts = JSON.parse(localStorage.getItem("soldProducts"));
-      updateSoldProducts();
-    }
-  
-    if (localStorage.getItem("refillProducts")) {
-      refillProducts = JSON.parse(localStorage.getItem("refillProducts"));
-      updateRefillProducts();
-    }
-  
-    if (localStorage.getItem("totalMoney")) {
-      totalMoney = parseFloat(localStorage.getItem("totalMoney"));
-      updateTotalMoney();
-    }
-  });
+  }
+  return product.toLowerCase();
+}
 
-  // Initialize the page with the current stock and sold products
+// Delete a product from the stock
+function deleteProduct(product) {
+  if (confirm(`Are you sure you want to delete "${capitalizeFirstLetter(product)}"?`)) {
+    delete stock[product];
     updateStock();
-    updateSellProductOptions();
+    updateSellProductOptions(); // Update options in the sell-product dropdown
+    updateRefillProducts();
+
+    // Store the updated data in Local Storage
+    localStorage.setItem("stock", JSON.stringify(stock));
+    localStorage.setItem("soldProducts", JSON.stringify(soldProducts));
+    localStorage.setItem("refillProducts", JSON.stringify(refillProducts));
+    localStorage.setItem("totalMoney", JSON.stringify(totalMoney));
+  }
+}
+
+// Capitalize the first letter of a string
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Update the stock list display
+function updateStock() {
+  const stockList = document.getElementById("stock-list");
+  stockList.innerHTML = "";
+
+  for (const product in stock) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+      <span contenteditable="true" class="product-name" data-product="${product}">${capitalizeFirstLetter(product)}</span>: 
+      <span class="product-quantity" id="stock-${product}">${stock[product]}</span>
+      <button onclick="deleteProduct('${product}')">Remove</button>
+    `;
+    stockList.appendChild(listItem);
+  }
+}
+
+// When the page loads, retrieve data from Local Storage if available
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("stock")) {
+    stock = JSON.parse(localStorage.getItem("stock"));
+    updateStock();
+  }
+
+  if (localStorage.getItem("soldProducts")) {
+    soldProducts = JSON.parse(localStorage.getItem("soldProducts"));
     updateSoldProducts();
+  }
+
+  if (localStorage.getItem("refillProducts")) {
+    refillProducts = JSON.parse(localStorage.getItem("refillProducts"));
+    updateRefillProducts();
+  }
+
+  if (localStorage.getItem("totalMoney")) {
+    totalMoney = parseFloat(localStorage.getItem("totalMoney"));
     updateTotalMoney();
+  }
+});
+
+// Initialize the page with the current stock and sold products
+updateStock();
+updateSellProductOptions();
+updateSoldProducts();
+updateTotalMoney();
