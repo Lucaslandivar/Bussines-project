ef6b2b536db1e64e94c386214771ae11f933bc9d
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-08-03 16:41:25 -0300

    Some functions in the money section.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,29 @@
 // Money Section
+// Seleção de Elementos
+const totalMoney = document.getElementById("totalMoney");
+const moneyAmountInput = document.getElementById("money-amount");
+const moneySourceInput = document.getElementById("money-source");
+const transactionList = document.getElementById("transactionsList");
+const moneyBtn = document.getElementById("moneyBtn");
+
+
+// Funções
+function addTransaction() {
+    const moneyAmount = moneyAmountInput.value;
+    const moneySource = moneySourceInput.value;
+    const moneyLi = document.createElement("li");
+
+    if (moneyAmountInput.value === '' || moneySourceInput.value === '') {
+        alert("Valores incorretos!")
+    } else {
+        moneyLi.innerHTML = `$${moneyAmount} foram adicionados de: ${moneySource}`;
+        moneyLi.classList.add("moneyLi");
+        transactionList.appendChild(moneyLi);
+    }
+};
+// Eventos
+moneyBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    addTransaction();
+});
