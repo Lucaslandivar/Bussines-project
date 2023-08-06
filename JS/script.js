96648a7debc43afcc0c056549a34e0ec9f59cc46
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-08-06 11:26:20 -0300

    I fix some bugs in the money section.

--- JS/script.js
+++ JS/script.js
@@ -9,25 +9,24 @@ const moneyBtn = document.getElementById("moneyBtn");
 
 // Funções
 function addTransaction() {
-    const moneyAmount = moneyAmountInput.value;
+    const moneyAmount = parseFloat(moneyAmountInput.value);
     const moneySource = moneySourceInput.value;
     const moneyLi = document.createElement("li");
-    const moneySpan = document.getElementsByClassName("negativeMoneyTransaction");
 
     if (moneyAmount >= 0) {
         moneyLi.innerHTML = `<span class="positiveMoneyTransaction">$${moneyAmount}</span> foram adicionados de: <span class="transactionInfo">${moneySource}</span>`;
-        moneyLi.classList.add("moneyLi");
-        transactionList.appendChild(moneyLi);
     } else {
-        moneyLi.innerHTML = `<span class="negativeMoneyTransaction">-$${moneyAmount}</span> foram descontados por: <span class="transactionInfo">${moneySource}</span>`;
-        moneyLi.classList.add("moneyLi");
-        transactionList.appendChild(moneyLi);
+        moneyLi.innerHTML = `<span class="negativeMoneyTransaction">-$${Math.abs(moneyAmount)}</span> foram descontados por: <span class="transactionInfo">${moneySource}</span>`;
     }
 
-    if (moneyAmountInput.value === '' || moneySourceInput.value === '') {
-        alert("Valores incorretos!")
+    moneyLi.classList.add("moneyLi");
+    transactionList.appendChild(moneyLi);
+
+    if (isNaN(moneyAmount) || moneyAmountInput.value === '' || moneySourceInput.value === '') {
+        alert("Valores incorretos!");
     } else {
-        return;
+        moneyAmountInput.value = ''; 
+        moneySourceInput.value = '';
     }
 };
 
@@ -36,4 +35,5 @@ moneyBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     addTransaction();
+
 });
