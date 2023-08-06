834158d56f264d20be29e81211ad77513d213e3d
Lucas Landivar <119813555+Lucaslandivar@users.noreply.github.com>
2023-08-06 11:10:50 -0300

    Fix some errors.

--- JS/script.js
+++ JS/script.js
@@ -12,15 +12,25 @@ function addTransaction() {
     const moneyAmount = moneyAmountInput.value;
     const moneySource = moneySourceInput.value;
     const moneyLi = document.createElement("li");
+    const moneySpan = document.getElementsByClassName("negativeMoneyTransaction");
+
+    if (moneyAmount >= 0) {
+        moneyLi.innerHTML = `<span class="positiveMoneyTransaction">$${moneyAmount}</span> foram adicionados de: <span class="transactionInfo">${moneySource}</span>`;
+        moneyLi.classList.add("moneyLi");
+        transactionList.appendChild(moneyLi);
+    } else {
+        moneyLi.innerHTML = `<span class="negativeMoneyTransaction">-$${moneyAmount}</span> foram descontados por: <span class="transactionInfo">${moneySource}</span>`;
+        moneyLi.classList.add("moneyLi");
+        transactionList.appendChild(moneyLi);
+    }
 
     if (moneyAmountInput.value === '' || moneySourceInput.value === '') {
         alert("Valores incorretos!")
     } else {
-        moneyLi.innerHTML = `$${moneyAmount} foram adicionados de: ${moneySource}`;
-        moneyLi.classList.add("moneyLi");
-        transactionList.appendChild(moneyLi);
+        return;
     }
 };
+
 // Eventos
 moneyBtn.addEventListener("click", (e) => {
     e.preventDefault();
